d107009d8634833bde7c89a9ee99644802f67842
Yabdiel1 <yabdiel.ocasio@upr.edu>
2023-10-01 23:13:14 -0400

    Semi-completed page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,97 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
+import pic from '../../public/pngfind.com-anime-png-gif-6822199.png';
+import pacimage from '../../public/Pacman-Game-Wall-Mural.jpg';
+import bloompic from '../../public/0_X8V9OosecAyGjb97.jpg';
+import audiopic from '../../public/maxresdefault.jpg';
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className={darkMode ? 'dark' : ""}>
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900"
+      >
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-lg text-transparent bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text font-extrabold">developedbysGHugz</h1>
+            <ul className="flex items-center px-2">
+              <li><BsFillMoonStarsFill  onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-gray-300"/></li>
+              <li><a className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8">Resume</a></li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Yabdiel Ocasio</h2>
+            <h3 className="text-2xl py-2 md:text-3xl dark:text-gray-300">College Software Engineer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-lg dark:text-gray-300">
+              Hi, my name is<br></br>Yabdiel A. Ocasio Cese<br></br>I am a Programmer
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 pb-10 pt-3 text-gray-600 dark:text-gray-300">
+            <AiFillGithub/>
+            <AiFillLinkedin/>
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full h-80 w-80 overflow-hidden md:h-96 md:w-96">
+            <Image src={pic} layout="fill" objectFit="cover" alt={''}/>
+          </div>
+        </section>
+        <section className="min-h-screen">
+          <div className="text-center py-10 md:py-20">
+            <h2 className="text-2xl md:text-5xl py-2 md:py-4 font-extrabold dark:text-gray-300">Projects</h2>
+            <h3 className="dark:text-gray-300">Things I've built so far</h3>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="max-w-sm rounded-md overflow-hidden shadow-lg mx-auto my-10 max-xl dark:bg-gray-800">
+              <Image className="w-full" src={pacimage} alt='Pacman game'/>
+              <div className="px-6 py-4">
+                <h2 className="text-lg font-medium pt-2 pb-2 dark:text-gray-300">Pacman Game</h2>
+                <p className="py-2 dark:text-gray-300">
+                  Constructed Pac-man game, given by a template. The goal of the project was to add functionality to the game by implementing varios features like: GPS, powerups, etc.
+                </p>
+              </div>
+              <div className="px-6 pt-1 pb-2">
+                <h3 className="pb-2 text-teal-600">Language Used:</h3>
+                <p className="text-gray-800 py-2 dark:text-gray-300">C++</p>
+              </div>
+            </div>
+            <div className="max-w-sm rounded-md overflow-hidden shadow-lg mx-auto my-10 dark:bg-gray-800">
+              <Image className="w-full" src={bloompic} alt='Bloomfilter'/>
+              <div className="px-6 py-4">
+                <h2 className="text-lg font-medium pt-2 pb-2 dark:text-gray-300">Audio Visualizer</h2>
+                <p className="py-2 dark:text-gray-300">
+                Implemented a class which measured the wavelength of the wav files to produce different
+                size of the rectangles to simulate the sound.
+                </p>
+              </div>
+              <div className="px-6 pt-1 pb-2">
+                <h3 className="pb-2 text-teal-600">Language Used:</h3>
+                <p className="text-gray-800 py-2 dark:text-gray-300">C++</p>
+              </div>
+            </div>
+            <div className="max-w-sm rounded-md overflow-hidden shadow-lg mx-auto my-10 dark:bg-gray-800">
+              <Image className="w-full" src={audiopic} alt='Audio Visualizer'/>
+              <div className="px-6 py-4">
+                <h2 className="text-lg font-medium pt-2 pb-2 dark:text-gray-300">Bloom Filter</h2>
+                <p className="py-2 dark:text-gray-300">
+                  A Bloom Filter is a compact data structure used for quick and approximate membership
+                  queries. It hashes elements to a bit array, initially set to 0. By applying multiple hash
+                  functions, elements are added to the filter at various positions. When checking for
+                  membership, the same hashes are used, and if corresponding bits are set to 1, the element
+                  is likely part of the set.
+                </p>
+              </div>
+              <div className="px-6 pt-1 pb-2">
+                <h3 className="pb-2 text-teal-600">Language Used:</h3>
+                <p className="text-gray-800 py-2 dark:text-gray-300">Python</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   )
 }
